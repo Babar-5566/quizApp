c650b93504292f71d1fc62ebe6cc74604afb841a
Babar-5566 <skbabarali5566@gmail.com>
2025-06-18 20:03:59 +0530

    Now the loader appears and the response is collected and stored from opentdb.com

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 const category = document.querySelector("#trivia_category");
 const difficulty = document.querySelector("#trivia_difficulty");
 const type = document.querySelector("#trivia_type");
-const APILink = makeAPILink(category.value,difficulty.value,type.value);
+const choosePage = document.querySelector(".choosePage");
+const loader = document.querySelector(".loader");
+const quizQuestions = document.querySelector(".quizQuestions")
 
 //making the api link
 function makeAPILink(category,difficulty,type) {
@@ -22,20 +24,50 @@ function makeAPILink(category,difficulty,type) {
 document.querySelector("form button").addEventListener("click", (e) => {
     e.preventDefault();
     // console.log(category,difficulty,type);
+    const APILink = makeAPILink(category.value,difficulty.value,type.value);
     console.log(APILink);
     
     //resetting all values of select tags
     category.value = "any";
     difficulty.value = "any";
     type.value = "any";
-
+    
     //Removing choosePage
-    document.querySelector("#choosePage").classList.add("hide");
+    choosePage.classList.add("hide");
 
     //showing loader
-    document.querySelector("#loader").classList.add("show");
+    loader.classList.add("show");
     
-    //show animation
-    // document.querySelector("#quizQuestions").classList.add("moveAnimation");
+    startQuizProcessMCQ(APILink);
 });
 
+const questionsSet = [];
+async function startQuizProcessMCQ(APILink) {
+    const response = await fetch(APILink);
+    const data = await response.json();
+    console.log(data.results);
+    (data.results).forEach(element => {
+        // choices = [];
+        // // correctAnsIndex = Math.floor(Math.random()*4);
+        // for (let index = 0; index < 4; index++) {
+            //     if (correctAnsIndex === index) {
+                //         choices.push(element.correct_answer);
+                //         continue;
+                //     }
+                //     choices.push(element.incorrect_answers[k++]);
+                // }
+                // console.log(choices);
+                questionsSet.push(
+                    {
+                        question: `${element.question}`,
+                        incorrect_choices: `${element.incorrect_answers}`,
+                        correct_choice: `${element.correct_answer}`,
+                    }
+                )        
+            });
+            console.log(questionsSet);
+            
+            //hiding loader and showing questions
+            loader.classList.add("hide");
+            quizQuestions.classList.add("moveAnimation");
+        }
